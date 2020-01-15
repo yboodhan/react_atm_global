89bbd7140115d77d8c12d2cd60db84063f055c44
yboodhan <yboodhan@gmail.com>
2020-01-14 22:44:02 -0800

    completed hw

--- src/Account.js
+++ src/Account.js
@@ -9,19 +9,43 @@ export default class Account extends Component {
     }
 
     this.handleDepositClick = this.handleDepositClick.bind(this)
+    this.handleWithdrawalClick = this.handleWithdrawalClick.bind(this)
   }
 
   handleDepositClick(e) {
     e.preventDefault();
-    if (isNaN(this.refs.amount.value)) {
-      console.log("Not a number");
-    }
-    else {
-      let amount = +this.refs.amount.value;
+    let amount = +this.refs.amount.value;
+    if (amount < 0) {
+      console.log('Cannot enter negative number!');
+    } else {
       let newBalance = this.state.balance + amount;
       this.setState({
         balance: newBalance
       })
+      if (newBalance < 0) {
+        this.setState({
+          balance: 0
+        })
+      }
+      this.refs.amount.value = '';
+    }
+  }
+
+  handleWithdrawalClick(e) {
+    e.preventDefault();
+    let amount = +this.refs.amount.value;
+    if (amount < 0) {
+      console.log('Cannot enter negative number!');
+    } else {
+      let newBalance = this.state.balance - amount;
+      this.setState({
+        balance: newBalance
+      })
+      if (newBalance < 0) {
+        this.setState({
+          balance: 0
+        })
+      }
       this.refs.amount.value = '';
     }
   }
@@ -36,8 +60,9 @@ export default class Account extends Component {
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className={balanceClass}>${this.state.balance}</div>
-        <input type="text" placeholder="enter an amount" ref="amount" />
+        <input type="number" min="1" placeholder="enter an amount" ref="amount" />
         <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawalClick} />
       </div>
     )
   }
